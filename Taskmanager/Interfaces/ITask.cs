f80f273644b5143f4eecf3e007743175df4b5b3f
erdem <45831228+erdemcobanoglu@users.noreply.github.com>
2024-05-10 13:52:17 +0300

    added enum and interfaces

---
+++ Taskmanager/Interfaces/ITask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal interface ITask
+    {
+        /// <summary>
+        /// Gets the name of task
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets the description of the task
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Gets the current status of the task.
+        /// </summary>
+        TaskStatus Status { get; }
+
+        /// <summary>
+        /// Runs the task
+        /// </summary>
+        void Run();
+
+        /// <summary>
+        /// Cancels the running task if applicable
+        /// </summary>
+        void Cancel();
+
+        /// <summary>
+        /// Sets the trigger for when the task should run automatically.
+        /// </summary>
+        /// <param name="trigger">The trigger object defining the schedule.</param>
+        void SetTrigger(ITrigger trigger);
+
+
+        public interface ITrigger
+        {
+            // Define properties and method 
+        }
+
+    }
+}
---
+++ Taskmanager/Interfaces/ITrigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal interface ITrigger
+    {
+        TriggerType Type { get; }
+
+        // Define method for specific trigger types
+        void SetDailyTrigger(int interval = 1);
+        void SetHourlyTrigger(int interval = 1);
+        void SetAtSpecificTime(DateTime time);
+        void SetWeeklyTrigger(DayOfWeek dayOfWeek, int interval = 1);
+        void SetMonthlyTrigger(int dayOfMonth, int interval = 1);
+        void SetYearlyTriger(int month, int dayOfMonth, int interval = 1);
+
+
+    }
+}
